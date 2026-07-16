258d24f6259edf0f9edfedd42c7b5011c9ef36d0
Zaine Mac <zaineirshad.07@gmail.com>
2026-07-16 07:35:07 +0100

    File for managing user profile

---
+++ User.java
@@ -0,0 +1,20 @@
+class User {
+    String name;
+    int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
