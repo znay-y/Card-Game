17df212791b9bb1b748d0865480147c17768f8e8
Zaine Mac <zaineirshad.07@gmail.com>
2026-07-29 16:42:36 +0100

    Class for user profiles (V1.3)

--- User.java
+++ User.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class User {
     String name;
     int chips;
@@ -25,6 +27,34 @@ class User {
 
     public void setCurrentBet(int currentBet) {
         this.currentBet = currentBet;
+        IO.print("\nThe current bet is " + currentBet+"\n");
     }
-    
+
+    public void profile(Scanner sc){
+        IO.clear();
+        IO.print("User Profile");
+        IO.print("Name: " + this.getName());
+        IO.print("Chips: " + this.getChips());
+        IO.print("Press enter to continue");
+
+        sc.nextLine();
+        IO.clear();
+    }
+
+    public int setbet(Scanner sc) {
+        int betAmount = IO.INTput(sc, "How many chips are you betting");
+
+        while (betAmount <= 0 || betAmount > this.getChips()) {
+
+            if (betAmount > this.getChips()) {
+                IO.print("You cannot bet more than you have");
+            } else if (betAmount <= 0) {
+                IO.print("You cannot bet a non-positive amount");
+            }
+            betAmount = IO.INTput(sc, "How many chips are you betting");
+        }
+        setCurrentBet(betAmount);
+        return betAmount;
+    }
+
 }
